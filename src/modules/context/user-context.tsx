f022c3cb873b74a1a022aec5afddceade1e20bc7
Joao Paulo <jprp17@live.com>
2023-03-07 10:52:52 -0300

    fix: fix undefined error when getting data from localstorage

--- src/modules/context/user-context.tsx
+++ src/modules/context/user-context.tsx
@@ -1,13 +1,22 @@
-import React, { createContext, Dispatch, PropsWithChildren, SetStateAction, useEffect, useMemo, useRef, useState } from "react";
+import React, {
+  createContext,
+  Dispatch,
+  PropsWithChildren,
+  SetStateAction,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { UserI } from "../types/types";
 
 interface UserContextInterface {
-  user: UserI | null
-  isLogged: boolean
-  setUser: Dispatch<SetStateAction<UserI | null>>
+  user: UserI | null;
+  isLogged: boolean;
+  setUser: Dispatch<SetStateAction<UserI | null>>;
 }
 
-const UserContext = createContext<UserContextInterface | null>(null)
+const UserContext = createContext<UserContextInterface | null>(null);
 
 const UserProvider: React.FC<PropsWithChildren> = ({ children }) => {
   const initialRender = useRef(true);
@@ -15,10 +24,10 @@ const UserProvider: React.FC<PropsWithChildren> = ({ children }) => {
 
   // fetch data
   useEffect(() => {
-    const value = localStorage.getItem('match4action@user');
-    const user = !!value ? JSON.parse(value) : null;
-    setUser(user)
-  }, [])
+    const value = localStorage.getItem("match4action@user");
+    const user = value && value !== "undefined" ? JSON.parse(value) : null;
+    setUser(user);
+  }, []);
 
   useEffect(() => {
     if (initialRender.current) {
@@ -28,14 +37,16 @@ const UserProvider: React.FC<PropsWithChildren> = ({ children }) => {
     localStorage.setItem("match4action@user", JSON.stringify(user));
   }, [user]);
 
-  const isLogged = useMemo(() => user && Object.keys(user).length > 0 ? true : false, [user]);
+  const isLogged = useMemo(
+    () => (user && Object.keys(user).length > 0 ? true : false),
+    [user]
+  );
 
   return (
-    <UserContext.Provider value={{ user, setUser, isLogged, }}>
+    <UserContext.Provider value={{ user, setUser, isLogged }}>
       {children}
     </UserContext.Provider>
-  )
-}
+  );
+};
 
 export { UserProvider, UserContext };
-
