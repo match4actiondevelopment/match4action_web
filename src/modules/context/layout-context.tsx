00846657e50ca68ca3bbebab16606e4e73031b3b
Joao Paulo <jprp17@live.com>
2023-03-19 19:23:57 -0300

    feat: pass accessToken to head component and remove logic to get access token in home page from params

--- src/modules/context/layout-context.tsx
+++ src/modules/context/layout-context.tsx
@@ -1,46 +1,53 @@
-'use client';
+"use client";
 
-import { CacheProvider } from '@emotion/react';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import dynamic from 'next/dynamic';
-import { lato, sourceSerifPro } from '../styles/fonts';
-import theme from '../styles/theme';
-import createEmotionCache from './createEmotionCache';
-import { UserProvider } from './user-context';
-import React from "react";
+import { CacheProvider } from "@emotion/react";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import dynamic from "next/dynamic";
+import React, { useState } from "react";
+import { lato, sourceSerifPro } from "../styles/fonts";
+import theme from "../styles/theme";
+import createEmotionCache from "./createEmotionCache";
+import { UserProvider } from "./user-context";
 
 export interface LayoutContextProps {
   children: React.ReactNode;
+  accessToken?: string;
 }
 
-const Header = dynamic(() => import('../../modules/components/Header').then((doc) => doc.Header), {
-  ssr: false,
-});
+const Header = dynamic(
+  () => import("../../modules/components/Header").then((doc) => doc.Header),
+  {
+    ssr: false,
+  }
+);
 
 export const queryClient = new QueryClient();
 
-export const LayoutContext: React.FC<LayoutContextProps> = ({ children }) => {
+export const LayoutContext: React.FC<LayoutContextProps> = ({
+  children,
+  accessToken,
+}) => {
+  const [queryClientInstance] = useState(() => new QueryClient());
   const cache = createEmotionCache();
   return (
     <CacheProvider value={cache}>
-      <QueryClientProvider client={queryClient}>
+      <QueryClientProvider client={queryClientInstance}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <UserProvider>
             <Box
-              className={sourceSerifPro.className + '' + lato.className}
+              className={sourceSerifPro.className + "" + lato.className}
               position="relative"
               sx={{
-                overflowX: 'hidden',
-                height: '100%',
-                minHeight: '100vh',
+                overflowX: "hidden",
+                height: "100%",
+                minHeight: "100vh",
               }}
-
             >
-              <Header />
+              <Header accessToken={accessToken} />
               {children}
             </Box>
           </UserProvider>
--- src/modules/pages/HomePage.tsx
+++ src/modules/pages/HomePage.tsx
@@ -1,50 +1,23 @@
-'use client';
+"use client";
 
-import Box from '@mui/material/Box';
-import dynamic from 'next/dynamic';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { useContext, useEffect } from 'react';
-import { ContentfulResolver } from '../contentful/components/ContentfulResolver';
-import { PurpleItem } from '../contentful/queries/types';
-import { UserContext } from '../context/user-context';
-import { useFetchProfile } from '../hooks/useGetProfile';
+import Box from "@mui/material/Box";
+import dynamic from "next/dynamic";
+import { ContentfulResolver } from "../contentful/components/ContentfulResolver";
+import { PurpleItem } from "../contentful/queries/types";
 
 interface HomePageInterface {
   data: PurpleItem;
-  searchParams?: { [key: string]: string | string[] | undefined };
 }
 
 const ImageBackground = dynamic(
-  () => import('../components/ImageBackground').then((doc) => doc.ImageBackground),
+  () =>
+    import("../components/ImageBackground").then((doc) => doc.ImageBackground),
   {
     ssr: false,
   }
 );
 
 export default function HomePage({ data }: HomePageInterface) {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const { setUser } = useContext(UserContext) ?? {};
-
-  const access_token = searchParams.get('access_token');
-  const refresh_token = searchParams.get('refresh_token');
-  const user_id = searchParams.get('user_id');
-
-  useEffect(() => {
-    if (access_token && refresh_token && user_id) {
-      localStorage.setItem('match4action@tokens', JSON.stringify({ access_token, refresh_token }));
-    }
-  }, [access_token, refresh_token, router, searchParams, user_id]);
-
-  const { data: user } = useFetchProfile(user_id);
-
-  useEffect(() => {
-    if (user && user?._id) {
-      setUser && setUser(user);
-      router.push('/');
-    }
-  }, [user, setUser, router]);
-
   return (
     <>
       <ImageBackground />
