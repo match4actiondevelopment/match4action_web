cf182b26ee3bdde1945e16ce43deab4bf19d38e4
Joao Paulo <jprp17@live.com>
2023-03-19 19:22:03 -0300

    refact: logou user and redirect to home page when token is expired

--- src/modules/config/http.ts
+++ src/modules/config/http.ts
@@ -1,22 +1,12 @@
 // http.ts
-import axios from 'axios';
+import { logout } from "@/modules/services";
+import axios from "axios";
 
 const http = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_PATH,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
-});
-
-http.interceptors.request.use(async (config) => {
-  const match4actionTokens: { access_token: string; refresh_token: string } =
-    localStorage.getItem('match4action@tokens') && JSON.parse(localStorage.getItem('match4action@tokens')!);
-
-  const token = match4actionTokens?.access_token ?? match4actionTokens?.access_token;
-
-  config!.headers!.Authorization = `Bearer ${token}`;
-
-  return config;
 });
 
 http.interceptors.response.use(
@@ -24,19 +14,9 @@ http.interceptors.response.use(
     return response;
   },
   async (error) => {
-    if (error?.response?.status === 401) {
-      const authData: { access_token: string; refresh_token: string } = localStorage.getItem('match4action@tokens')
-        ? JSON.parse(localStorage.getItem('match4action@tokens')!)
-        : '';
-
-      const apiResponse = await axios.post(`${process.env.NEXT_PUBLIC_API_PATH}/auth/refreshToken`, {
-        refresh_token: authData?.refresh_token,
-      });
-
-      localStorage.setItem('match4action@tokens', JSON.stringify(apiResponse?.data?.data));
-
-      error.config.headers['Authorization'] = `bearer ${apiResponse?.data?.data?.access_token}`;
-      return axios(error.config);
+    if (error?.response?.status === 403) {
+      await logout();
+      return (window.location.href = window.location.origin);
     } else {
       return Promise.reject(error);
     }
