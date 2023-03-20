338d487c15d8cc6527b4b6850ce81ece8153d92e
Joao Paulo <jprp17@live.com>
2023-03-19 22:12:05 -0300

    refact: remove auth guard from forgot password page

--- src/modules/pages/ForgotPassword.tsx
+++ src/modules/pages/ForgotPassword.tsx
@@ -1,24 +1,12 @@
-'use client';
+"use client";
 
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
-import { useAuthGuard } from '../hooks/useAuthGuard';
-import { lato, sourceSerifPro } from '../styles/fonts';
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import { lato, sourceSerifPro } from "../styles/fonts";
 
 export default function ForgotPassword() {
-  const router = useRouter()
-  const { isLogged } = useAuthGuard()
-
-  useEffect(() => {
-    if (isLogged) {
-      router.push("/")
-    }
-  }, [isLogged, router])
-
   return (
     <Box
       component="main"
@@ -30,13 +18,13 @@ export default function ForgotPassword() {
       padding="0 1rem"
       height="100%"
       sx={(theme) => ({
-        [theme.breakpoints.down('sm')]: {
-          width: '100%',
+        [theme.breakpoints.down("sm")]: {
+          width: "100%",
         },
-        [theme.breakpoints.up('sm')]: {
-          width: '100%',
-          maxWidth: '540px',
-          margin: '2.75rem auto',
+        [theme.breakpoints.up("sm")]: {
+          width: "100%",
+          maxWidth: "540px",
+          margin: "2.75rem auto",
         },
       })}
     >
@@ -46,15 +34,15 @@ export default function ForgotPassword() {
         fontWeight={700}
         marginBottom="2rem"
         sx={(theme) => ({
-          [theme.breakpoints.down('sm')]: {
-            fontSize: '1.5rem',
-            lineHeight: '1.75rem',
+          [theme.breakpoints.down("sm")]: {
+            fontSize: "1.5rem",
+            lineHeight: "1.75rem",
             color: theme.palette.text.primary,
-            textAlign: 'center',
+            textAlign: "center",
           },
-          [theme.breakpoints.up('sm')]: {
-            fontSize: '2.5rem',
-            lineHeight: '2.5rem',
+          [theme.breakpoints.up("sm")]: {
+            fontSize: "2.5rem",
+            lineHeight: "2.5rem",
             color: theme.palette.text.primary,
           },
         })}
@@ -65,73 +53,78 @@ export default function ForgotPassword() {
         className={lato.className}
         marginBottom="1.5rem"
         sx={(theme) => ({
-          [theme.breakpoints.down('sm')]: {
-            fontSize: '0.875rem',
-            lineHeight: '1rem',
+          [theme.breakpoints.down("sm")]: {
+            fontSize: "0.875rem",
+            lineHeight: "1rem",
             color: theme.palette.text.primary,
-            textAlign: 'center',
+            textAlign: "center",
           },
-          [theme.breakpoints.up('sm')]: {
-            fontSize: '1rem',
-            lineHeight: '2.5rem',
+          [theme.breakpoints.up("sm")]: {
+            fontSize: "1rem",
+            lineHeight: "2.5rem",
             color: theme.palette.text.primary,
           },
         })}
       >
-        No worries, it happens :) Please enter your email below and we will send you instructions on how to reset your
-        password.
+        No worries, it happens :) Please enter your email below and we will send
+        you instructions on how to reset your password.
       </Typography>
-      <Box width="100%" display="flex" flexDirection="column" marginBottom="2.5rem">
+      <Box
+        width="100%"
+        display="flex"
+        flexDirection="column"
+        marginBottom="2.5rem"
+      >
         <TextField fullWidth label="Email" variant="outlined" size="small" />
       </Box>
       <Button
         className={lato.className}
         sx={(theme) => ({
-          [theme.breakpoints.down('sm')]: {
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '39px',
-            background: '#FFD15C',
+          [theme.breakpoints.down("sm")]: {
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "39px",
+            background: "#FFD15C",
             color: theme.palette.text.primary,
             fontWeight: 600,
-            fontSize: '1rem',
-            ':focus': {
-              background: '#FFD15C',
+            fontSize: "1rem",
+            ":focus": {
+              background: "#FFD15C",
             },
-            ':active': {
-              background: '#FFD15C',
+            ":active": {
+              background: "#FFD15C",
             },
-            textTransform: 'capitalize',
-            textDecoration: 'none',
-            minWidth: '100%',
-            borderRadius: '5px',
-            marginBottom: '1rem',
-            cursor: 'pointer',
+            textTransform: "capitalize",
+            textDecoration: "none",
+            minWidth: "100%",
+            borderRadius: "5px",
+            marginBottom: "1rem",
+            cursor: "pointer",
           },
-          [theme.breakpoints.up('sm')]: {
-            display: 'inline-block',
-            padding: '14px 28px',
-            background: '#FFD15C',
+          [theme.breakpoints.up("sm")]: {
+            display: "inline-block",
+            padding: "14px 28px",
+            background: "#FFD15C",
             color: theme.palette.text.primary,
             fontWeight: 600,
-            boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.15)',
-            fontSize: '1rem',
-            ':focus': {
-              background: '#FFD15C',
+            boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.15)",
+            fontSize: "1rem",
+            ":focus": {
+              background: "#FFD15C",
             },
-            ':active': {
-              background: '#FFD15C',
+            ":active": {
+              background: "#FFD15C",
             },
-            ':hover': {
-              background: '#FFD15C',
+            ":hover": {
+              background: "#FFD15C",
             },
-            textTransform: 'capitalize',
-            textDecoration: 'none',
-            borderRadius: '5px',
-            marginBottom: '1rem',
-            cursor: 'pointer',
-            width: '100%',
+            textTransform: "capitalize",
+            textDecoration: "none",
+            borderRadius: "5px",
+            marginBottom: "1rem",
+            cursor: "pointer",
+            width: "100%",
           },
         })}
       >
