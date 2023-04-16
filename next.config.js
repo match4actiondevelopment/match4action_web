346bdd45e37bda14fba64c14ce2fb2eb6bb1723b
Joao Paulo <jprp17@live.com>
2023-04-16 15:23:32 -0500

    chore: upgrade next js version

--- next.config.js
+++ next.config.js
@@ -4,34 +4,38 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**.images.ctfassets.net',
+        protocol: "https",
+        hostname: "**.images.ctfassets.net",
       },
       {
-        protocol: 'https',
-        hostname: '**.lh3.googleusercontent.com',
+        protocol: "https",
+        hostname: "**.lh3.googleusercontent.com",
       },
       {
-        protocol: 'https',
-        hostname: '**.firebasestorage.googleapis.com',
+        protocol: "https",
+        hostname: "**.firebasestorage.googleapis.com",
       },
     ],
-    domains: ['images.ctfassets.net', 'lh3.googleusercontent.com', 'firebasestorage.googleapis.com'],
+    domains: [
+      "images.ctfassets.net",
+      "lh3.googleusercontent.com",
+      "firebasestorage.googleapis.com",
+    ],
   },
   experimental: {
     appDir: true,
-    fontLoaders: [
-      { loader: '@next/font/google', options: { subsets: ['latin'] } },
-    ],
+    // fontLoaders: [
+    //   { loader: '@next/font/google', options: { subsets: ['latin'] } },
+    // ],
   },
   modularizeImports: {
-    '@mui/material': {
-      transform: '@mui/material/{{member}}',
+    "@mui/material": {
+      transform: "@mui/material/{{member}}",
     },
-    '@mui/icons-material': {
-      transform: '@mui/icons-material/{{member}}',
+    "@mui/icons-material": {
+      transform: "@mui/icons-material/{{member}}",
     },
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
