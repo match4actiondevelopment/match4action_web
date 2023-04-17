4527723dca7bc19475434c3691df125646c7c1a7
João Paulo Rodrigues <jprp17@live.com>
2023-04-16 22:08:17 -0500

    fix: make ImageBackground component ssr false (#3)

--- src/modules/pages/GenericPage.tsx
+++ src/modules/pages/GenericPage.tsx
@@ -1,13 +1,21 @@
 "use client";
 
-import { ImageBackground } from "@/modules/components/ImageBackground";
 import Box from "@mui/material/Box";
+import dynamic from "next/dynamic";
 import { ContentfulResolver } from "../contentful/components/ContentfulResolver";
 import { PurpleItem } from "../contentful/queries/types";
 
 interface HomePageInterface {
   data: PurpleItem;
 }
+
+const ImageBackground = dynamic(
+  () =>
+    import("../components/ImageBackground").then((doc) => doc.ImageBackground),
+  {
+    ssr: false,
+  }
+);
 
 export default function GenericPage({ data }: HomePageInterface) {
   const heroImage =
