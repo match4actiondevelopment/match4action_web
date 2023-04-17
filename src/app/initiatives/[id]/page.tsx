df6867ad0013e9239a09aae558c71f89cd393abf
João Paulo Rodrigues <jprp17@live.com>
2023-04-16 23:10:24 -0500

    fix: remove head.js file from initiative page (#6)

--- src/app/initiatives/[id]/page.tsx
+++ src/app/initiatives/[id]/page.tsx
@@ -1,4 +1,18 @@
 import VolunteerPost from "@/modules/pages/VolunteerPost";
+import { fetchInitiative } from "@/modules/services";
+import { Metadata } from "next/dist/lib/metadata/types/metadata-interface";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string };
+}): Promise<Metadata> {
+  const data = await fetchInitiative(params.id);
+
+  return {
+    title: data ? `Match4purpose - ${data?.initiativeName} Initiative` : "",
+  };
+}
 
 export default function Page({ params }: { params: { id: string } }) {
   return <VolunteerPost params={params} />;
